278000f819a576a307650b3bbe8999a9973bc657
Fabrizio Lungo <fab@lungo.co.uk>
2012-03-04 14:41:56 +0000

    Void damage totally removed.

---
+++ VoidWarp/src/me/flungo/bukkit/VoidWarp/PlayerListeners.java
@@ -0,0 +1,36 @@
+package me.flungo.bukkit.VoidWarp;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class PlayerListeners implements Listener {
+	public static VoidWarp plugin;
+	
+	public PlayerListeners(VoidWarp instance) {
+		plugin = instance;
+	}
+
+	@EventHandler
+	public void onPlayerMove(PlayerMoveEvent event) {
+		Player p = event.getPlayer();
+		World w = p.getWorld();
+		Location to = event.getTo();
+		int y = to.getBlockY();
+		if (y <= -50) {
+			Location spawn = new Location(w, w.getSpawnLocation().getX(), w.getHighestBlockYAt(w.getSpawnLocation())+2, w.getSpawnLocation().getZ());
+			p.teleport(spawn);
+			p.setFallDistance(0);
+		}
+	}
+	
+	@EventHandler
+	public void onVoidDamage(EntityDamageEvent event) {
+		if (event.getCause() == EntityDamageEvent.DamageCause.VOID)
+			event.setCancelled(true);
+	}
+}
